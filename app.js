8d6f922b53b8bf5f9f22b4450f902cbd5716b958
Farooq Ahmed <03174748082ali@gmail.com>
2025-03-09 23:32:14 +0500

    my second commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % === modulus
+var num1 = +prompt('Enter Your First Name')
+var num2 = +prompt('Enter Your Second Number')
+var Operator = prompt('+ - * / %')
